bc38ce6148e5b556166a697599557e0baf405af2
Lidia Gianne <lidia_pokemon@yahoo.com.br>
2016-04-16 15:37:24 -0300

    Update Teste.java

--- Arvores/Teste.java
+++ Arvores/Teste.java
@@ -4,13 +4,13 @@ public class Teste {
     public static void main (String[] args){
         System.out.println("Árvore AVL");
         ArvoreAvl a = new ArvoreAvl();
-        /*a.inserir(8);
+        a.inserir(8);
         a.inserir(9);
         a.inserir(7);
         a.inserir(2);
         a.inserir(10);
         a.inserir(6);
-        a.inserir(11);*/
+        a.inserir(11);
         System.out.println(a.printar());
         System.out.println("---------------------------------");
         System.out.println("Árvore Rubro Negra");
